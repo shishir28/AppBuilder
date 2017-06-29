c01b706eb2d2971946c45e1a4c2e85e3f31d8f4b
shishir28 <shishir28@gmail.com>
2017-06-29 21:15:23 +1000

    View Role upper screen done

--- src/Monad.AB.Web.ng/src/app/roles/view-role.component.ts
+++ src/Monad.AB.Web.ng/src/app/roles/view-role.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MdSnackBar } from "@angular/material";
+import { DialogService } from '../widgets/dialogs/dialog.service';
+
+import { RolesService } from './shared/roles.service';
+import { RoleViewModel } from './shared/role';
 
 @Component({
-  selector: 'ms-view-role',
-  templateUrl: './view-role.component.html',
-  styleUrls: ['./view-role.component.scss']
+    selector: 'ms-view-role',
+    templateUrl: './view-role.component.html',
+    styleUrls: ['./view-role.component.scss']
 })
 export class ViewRoleComponent implements OnInit {
+    formGroup: FormGroup;
+    role: RoleViewModel = new RoleViewModel();
+    serverErrorMessage: string;
+    roleId: string;
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private snackBar: MdSnackBar,
+        private dialogService: DialogService,
+        private rolesService: RolesService) {
+    }
 
-  constructor() { }
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.roleId = params['roleid'];
+            if (!this.roleId)
+                return;
 
-  ngOnInit() {
-  }
+            this.rolesService.getRole(this.roleId)
+                .subscribe(
+                role => {
+                    this.role = role;
+                },
+                response => {
+                    if (response.status == 404) {
+                        this.router.navigate(['NotFound']);
+                    }
+                });
+        });
+    }
 
+    editRole(roleId): void {
+        this.router.navigateByUrl('/roles/edit/' + roleId);
+    }
+
+    deleteRole(roleId): void {
+        this.dialogService.confirm('Delete Role', 'Are you sure want to delete this role?')
+            .subscribe(result => {
+                let res: any = result;
+                if (res == 'yes') {
+                    this.rolesService.deleteRole(roleId)
+                        .subscribe(response => {
+                            if (response.statusCode == 204) {
+                                let snackBarRef = this.snackBar.open('Role deleted Successfully!', 'Close', {
+                                    duration: 500
+                                });
+                                snackBarRef.afterDismissed().subscribe(() => {
+                                    this.router.navigateByUrl('/roles');
+                                });
+
+                            } else if (response.statusCode == 412) {
+                                this.serverErrorMessage = "Some details were missing!";
+                            } else {
+                                this.serverErrorMessage = response.content;
+                            }
+                        });
+                }
+            });
+    }
+
+    cancelChanges(e) {
+        this.router.navigateByUrl('/roles');
+    }
 }
